471f9e4e52d8212a393819c0d181bd0d5e902edb
enghddd <81799517+jung18@users.noreply.github.com>
2025-04-15 01:20:47 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+lis = [arr[0]]
+
+
+def binary_search(key, start, end):
+    while start < end:
+        middle = (start + end) // 2
+        if key == lis[middle] or lis[middle-1] < key < lis[middle]:
+            return middle
+        elif key < lis[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+    return start 
+
+
+for i in range(1, N):
+    if lis[-1] < arr[i]:
+        lis.append(arr[i])
+    else:
+        lis[binary_search(arr[i], 0, len(lis)-1)] = arr[i]
+
+print(len(lis))
