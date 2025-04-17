6e0ec638f304e56b56709e1100f53b3981583ab9
enghddd <81799517+jung18@users.noreply.github.com>
2025-04-17 22:16:49 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [[] for _ in range(n)]
+
+for i in range(0, n):
+    arr[i] = int(input())
+ 
+dp = [0 for _ in range(n)]
+dp[0] = arr[0]
+
+if n > 1:
+    dp[1] = arr[0] + arr[1]
+if n > 2:
+    dp[2] = max(dp[1], arr[0] + arr[2], arr[1] + arr[2])
+if n > 3:
+    for i in range(3, n):
+        dp[i] = max(dp[i-1], dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i])
+
+print(dp[n-1])
